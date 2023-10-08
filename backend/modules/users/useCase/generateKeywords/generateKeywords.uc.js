ea0882909a87cafce76e59094695b0c64a934d02
jose <ortegajoseweb02@gmail.com>
2023-10-08 13:25:43 -0300

    fix: delete apikey

--- backend/modules/users/useCase/generateKeywords/generateKeywords.uc.js
+++ backend/modules/users/useCase/generateKeywords/generateKeywords.uc.js
@@ -1,39 +1,42 @@
 import fetch from "node-fetch";
 
 class GenerateKeywordsUc {
-    async exec({motivation, description, title, topic}){
+  async exec({ motivation, description, title, topic }) {
+    let resulted;
 
-            let resulted;
+    const raw = JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Se necesita generar etiquetas generales para una base de datos basada en el siguiente
+     texto. Además de palabras clave relacionadas con ${topic}, también se deben considerar aspectos éticos y legales. Proporciona tres palabras clave, lo más generalistas posible, que reflejen tanto los avances científicos como los dilemas éticos involucrados en el texto a continuacion. Cada etiqueta debe constar de una sola palabra. Aqui tienes unos datos que te ayudaran con el contexto del proyecto: Titulo: ${title} Texto: ${description} Tema: ${topic} Motivacion (el porque se hace el proyecto): ${motivation}`,
+        },
+      ],
+    });
 
-            const raw = JSON.stringify({
-    "model": "gpt-3.5-turbo",
-    "messages": [ {
-    "role": "user", 
-    "content": "Se necesita generar etiquetas generales para una base de datos basada en el siguiente texto sobre avances científicos en medicina regenerativa y clonación terapéutica. Además de palabras clave relacionadas con la ciencia y la medicina, también se deben considerar aspectos éticos y legales. Proporciona tres palabras clave, lo más generalistas posible, que reflejen tanto los avances científicos como los dilemas éticos involucrados en el texto a continuacion. Cada etiqueta debe constar de una sola palabra:"
-    }]  
-});
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+        `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: raw,
+      redirect: "follow",
+    };
 
-            const requestOptions = {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer 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"
-                },
-                body: raw,
-                redirect: 'follow'
-            };
+    await fetch("https://api.openai.com/v1/chat/completions", requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        console.log({ result });
+        resulted = result;
+      })
+      .catch((error) => console.log("error", error));
 
-            await fetch("https://api.openai.com/v1/chat/completions", requestOptions)
-                .then(response => response.text())
-                .then(result => {
-                    console.log({result})
-                    resulted = result
-                })
-                .catch(error => console.log('error', error));
-
-            console.log({resulted})
-            return resulted;
-    }
+    console.log({ resulted });
+    return resulted;
+  }
 }
 
 export default GenerateKeywordsUc;
