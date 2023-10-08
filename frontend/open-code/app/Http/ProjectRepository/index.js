45a5ec456be3b1b9f034010e4b3abe7f73474f02
jose <ortegajoseweb02@gmail.com>
2023-10-08 11:55:13 -0300

    feat: add repository

---
+++ frontend/open-code/app/Http/ProjectRepository/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const url = 'https://6521d110a4199548356d95af.mockapi.io/api/v1/'
+
+class ProjectRepository {
+    async getOne(id){
+        return axios.get(`${url}/proyects/${id}`)
+    }
+    async getAll(){
+        return axios.get(`${url}/proyects`)
+    }
+    async deleteOne(id){
+        return axios.delete(`${url}/proyects/${id}`)
+    }
+    async updateOne(id) {
+        return axios.put(`${url}/proyects/${id}`)
+    }
+}
+
+export default ProjectRepository;
