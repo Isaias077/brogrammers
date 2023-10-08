a65b85e30131e7566358f515352b5f2352e7cd83
jose <ortegajoseweb02@gmail.com>
2023-10-08 10:31:33 -0300

    login implements

---
+++ frontend/open-code/app/components/login/googleLogin.jsx
@@ -0,0 +1,42 @@
+"use client"
+import { useRouter } from 'next/navigation';
+import { CheckUserExistence } from "./CheckUserExsistence";
+
+import { useAuth0 } from "@auth0/auth0-react";
+
+/**
+ * Component that renders login page
+ * @returns {JSX.Element} Element with login page
+ */
+function GoogleLogin() {
+    
+  const { user, isAuthenticated } = useAuth0();
+  /**
+   * Function that checks if user is exist in database
+   */
+  const handleChangeToken = () => {
+    CheckUserExistence(1).then((r) => {
+      const res = JSON.parse(r.response);
+      localStorage.setItem("avatar", res.avatar);
+      user.picture = res.avatar;
+    });
+  };
+  /**
+   * Function with the logic to redirect to the home page or dashboard
+   * @returns {JSX.Element} Navigate element
+   */
+  function comprobacion() {
+    if (JSON.parse(localStorage.getItem("id")) !== null) {
+      return redirect(`/`)
+    }
+    return redirect(`/callback`)
+  }
+  return (
+    <div>
+      {handleChangeToken(user)}
+      {isAuthenticated ? comprobacion() : null}
+    </div>
+  );
+}
+
+export default GoogleLogin;
